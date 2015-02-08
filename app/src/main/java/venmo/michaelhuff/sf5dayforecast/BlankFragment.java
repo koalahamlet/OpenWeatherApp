33924c3d34aeceeb6ff16a36198ae1d432c9d222
koalahamlet <koalahamlet@gmail.com>
2015-02-07 17:15:31 -0800

    add initial fragment work

--- app/src/main/java/venmo/michaelhuff/sf5dayforecast/BlankFragment.java
+++ app/src/main/java/venmo/michaelhuff/sf5dayforecast/BlankFragment.java
@@ -4,9 +4,30 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import javax.inject.Inject;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.converter.GsonConverter;
+import rx.Observer;
+import venmo.michaelhuff.sf5dayforecast.Models.ForcastResponseObject;
+import venmo.michaelhuff.sf5dayforecast.Models.WeatherOverview;
 
 
 /**
@@ -18,6 +39,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class BlankFragment extends android.support.v4.app.Fragment {
+
+    public String TAG = BlankFragment.class.getSimpleName();
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -26,6 +49,12 @@ public class BlankFragment extends android.support.v4.app.Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    @Inject
+    ApiClient apiClient;
+
+    @InjectView(R.id.listview)
+    ListView listView;
 
     private OnFragmentInteractionListener mListener;
 
@@ -64,76 +93,83 @@ public class BlankFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
-//
-//        //you have to butter your bread
-//        ButterKnife.inject(this);
-//
-//        // after the butterknife, comes the dagger
-//        // Perform injection so that when this call returns all dependencies will be available for use.
-//        WeatherApplication app = WeatherApplication.get(this);
-//        app.inject(this);
-//
-//        Observer<Response> observer = new Observer<Response>() {
-//            @Override
-//            public void onCompleted() {
-//
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//                Log.e(TAG, e.toString());
-//                if (e instanceof RetrofitError) {
-//                    RetrofitError error = (RetrofitError) e;
-//                    if (error.getResponse().getStatus() == 401) {
-//                        // something to handle a 401
-//                    }
-//                }
-//            }
-//
-//            @Override
-//            public void onNext(Response response) {
-//                int status = response.getStatus();
-//                ForcastResponseObject responseObject;
-//                ArrayList<WeatherOverview> forecast = new ArrayList<WeatherOverview>();
-//                Gson gson = new GsonBuilder()
-//                        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-//                        .create();
-//
-//                GsonConverter gsonConverter = new GsonConverter(gson);
-//                try {
-//                    responseObject = (ForcastResponseObject) gsonConverter.fromBody(response.getBody(), ForcastResponseObject.class);
-//                    forecast = (ArrayList) responseObject.getList();
-//                    Log.d("onNext", responseObject.toString());
-//                } catch (Exception e) {
-//                    onError(e);
-//                    Log.e("onNext", e.toString());
-//                    Log.e("onNext", e.getMessage());
-//                }
-//
-//                if (forecast != null) {
-//                    // if call was not null
-//                    // get a calendar instance
-//                    Calendar cal = Calendar.getInstance();
-//                    SimpleDateFormat sdf = new SimpleDateFormat("EEEE MMM dd,yyyy");
-//                    for (int i = 0; i < forecast.size(); i++) {
-//                        // then add the current date as a string to the array.
-//                        forecast.get(i).setDate(sdf.format(cal.getTime()));
-//                        //increment by one for the next time
-//                        cal.add(Calendar.DATE, 1);
-//                    }
-//                }
-//
-//                //Set the adapter
-//                ForecastAdapter adapter = new ForecastAdapter(getApplication().getApplicationContext(), forecast);
-//                listView.setAdapter(adapter);
-//
-//            }
-//
-//        };
-//
-//        apiClient.getForecast().subscribe(observer);
 
+
+        //you have to butter your bread
+        ButterKnife.inject(getActivity());
+
+        // after the butterknife, comes the dagger
+        // Perform injection so that when this call returns all dependencies will be available for use.
+        WeatherApplication app = WeatherApplication.get(getActivity());
+        app.inject(this);
+
+
+
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+        Observer<Response> observer = new Observer<Response>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, e.toString());
+                if (e instanceof RetrofitError) {
+                    RetrofitError error = (RetrofitError) e;
+                    if (error.getResponse().getStatus() == 401) {
+                        // something to handle a 401
+                    }
+                }
+            }
+
+            @Override
+            public void onNext(Response response) {
+                int status = response.getStatus();
+                ForcastResponseObject responseObject;
+                ArrayList<WeatherOverview> forecast = new ArrayList<WeatherOverview>();
+                Gson gson = new GsonBuilder()
+                        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                        .create();
+
+                GsonConverter gsonConverter = new GsonConverter(gson);
+                try {
+                    responseObject = (ForcastResponseObject) gsonConverter.fromBody(response.getBody(), ForcastResponseObject.class);
+                    forecast = (ArrayList) responseObject.getList();
+                    Log.d("onNext", responseObject.toString());
+                } catch (Exception e) {
+                    onError(e);
+                    Log.e("onNext", e.toString());
+                    Log.e("onNext", e.getMessage());
+                }
+
+                if (forecast != null) {
+                    // if call was not null
+                    // get a calendar instance
+                    Calendar cal = Calendar.getInstance();
+                    SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd, yyyy");
+                    for (int i = 0; i < forecast.size(); i++) {
+                        // then add the current date as a string to the array.
+                        forecast.get(i).setDate(sdf.format(cal.getTime()));
+                        //increment by one for the next time
+                        cal.add(Calendar.DATE, 1);
+                    }
+                }
+
+                //Set the adapter
+                ForecastAdapter adapter = new ForecastAdapter(getActivity(), forecast);
+                listView.setAdapter(adapter);
+
+            }
+        };
+
+        apiClient.getForecast().subscribe(observer);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/venmo/michaelhuff/sf5dayforecast/MainActivity.java
+++ app/src/main/java/venmo/michaelhuff/sf5dayforecast/MainActivity.java
@@ -1,37 +1,22 @@
 package venmo.michaelhuff.sf5dayforecast;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.widget.ListView;
-
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
 
 import javax.inject.Inject;
 
 import butterknife.ButterKnife;
-import butterknife.InjectView;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
-import retrofit.converter.GsonConverter;
-import rx.Observer;
-import venmo.michaelhuff.sf5dayforecast.Models.ForcastResponseObject;
-import venmo.michaelhuff.sf5dayforecast.Models.WeatherOverview;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements BlankFragment.OnFragmentInteractionListener{
 
     public String TAG = MainActivity.class.getSimpleName();
-    BlankFragment mDashBoardFragment;
+    BlankFragment blankFragment;
 
     FragmentManager fm;
     Fragment fragment;
@@ -39,8 +24,8 @@ public class MainActivity extends ActionBarActivity {
     @Inject
     ApiClient apiClient;
 
-    @InjectView(R.id.listview)
-    ListView listView;
+//    @InjectView(R.id.listview)
+//    ListView listView;
 
     // for fragment transaction
    // @InjectView(R.id.container)
@@ -50,7 +35,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // might not need setContentView
-        setContentView(R.layout.fragment_main);
+        setContentView(R.layout.activity_main);
 
         // hide that stuff
         ActionBar bar = getSupportActionBar();
@@ -59,8 +44,7 @@ public class MainActivity extends ActionBarActivity {
         if (savedInstanceState == null) {
             //TODO: do fragment transaction
             // to inflate fragment_main
-
-//            initMainContentFragment();
+            initMainContentFragment();
         }
 
         //you have to butter your bread
@@ -71,65 +55,65 @@ public class MainActivity extends ActionBarActivity {
         WeatherApplication app = WeatherApplication.get(this);
         app.inject(this);
 
-        Observer<Response> observer = new Observer<Response>() {
-            @Override
-            public void onCompleted() {
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.e(TAG, e.toString());
-                if (e instanceof RetrofitError) {
-                    RetrofitError error = (RetrofitError) e;
-                    if (error.getResponse().getStatus() == 401) {
-                        // something to handle a 401
-                    }
-                }
-            }
-
-            @Override
-            public void onNext(Response response) {
-                int status = response.getStatus();
-                ForcastResponseObject responseObject;
-                ArrayList<WeatherOverview> forecast = new ArrayList<WeatherOverview>();
-                Gson gson = new GsonBuilder()
-                        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                        .create();
-
-                GsonConverter gsonConverter = new GsonConverter(gson);
-                try {
-                    responseObject = (ForcastResponseObject) gsonConverter.fromBody(response.getBody(), ForcastResponseObject.class);
-                    forecast = (ArrayList) responseObject.getList();
-                    Log.d("onNext", responseObject.toString());
-                } catch (Exception e) {
-                    onError(e);
-                    Log.e("onNext", e.toString());
-                    Log.e("onNext", e.getMessage());
-                }
-
-                if (forecast != null) {
-                    // if call was not null
-                    // get a calendar instance
-                    Calendar cal = Calendar.getInstance();
-                    SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd, yyyy");
-                    for (int i = 0; i < forecast.size(); i++) {
-                        // then add the current date as a string to the array.
-                        forecast.get(i).setDate(sdf.format(cal.getTime()));
-                        //increment by one for the next time
-                        cal.add(Calendar.DATE, 1);
-                    }
-                }
-
-                //Set the adapter
-                ForecastAdapter adapter = new ForecastAdapter(getApplication().getApplicationContext(), forecast);
-                listView.setAdapter(adapter);
-
-            }
-
-        };
-
-        apiClient.getForecast().subscribe(observer);
+//        Observer<Response> observer = new Observer<Response>() {
+//            @Override
+//            public void onCompleted() {
+//
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                Log.e(TAG, e.toString());
+//                if (e instanceof RetrofitError) {
+//                    RetrofitError error = (RetrofitError) e;
+//                    if (error.getResponse().getStatus() == 401) {
+//                        // something to handle a 401
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onNext(Response response) {
+//                int status = response.getStatus();
+//                ForcastResponseObject responseObject;
+//                ArrayList<WeatherOverview> forecast = new ArrayList<WeatherOverview>();
+//                Gson gson = new GsonBuilder()
+//                        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+//                        .create();
+//
+//                GsonConverter gsonConverter = new GsonConverter(gson);
+//                try {
+//                    responseObject = (ForcastResponseObject) gsonConverter.fromBody(response.getBody(), ForcastResponseObject.class);
+//                    forecast = (ArrayList) responseObject.getList();
+//                    Log.d("onNext", responseObject.toString());
+//                } catch (Exception e) {
+//                    onError(e);
+//                    Log.e("onNext", e.toString());
+//                    Log.e("onNext", e.getMessage());
+//                }
+//
+//                if (forecast != null) {
+//                    // if call was not null
+//                    // get a calendar instance
+//                    Calendar cal = Calendar.getInstance();
+//                    SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd, yyyy");
+//                    for (int i = 0; i < forecast.size(); i++) {
+//                        // then add the current date as a string to the array.
+//                        forecast.get(i).setDate(sdf.format(cal.getTime()));
+//                        //increment by one for the next time
+//                        cal.add(Calendar.DATE, 1);
+//                    }
+//                }
+//
+//                //Set the adapter
+//                ForecastAdapter adapter = new ForecastAdapter(getApplication().getApplicationContext(), forecast);
+//                listView.setAdapter(adapter);
+//
+//            }
+//
+//        };
+//
+//        apiClient.getForecast().subscribe(observer);
 
     }
 
@@ -146,8 +130,12 @@ public class MainActivity extends ActionBarActivity {
 
     private Fragment createMainFragment() {
         Log.d(TAG, "createMainFragment");
-        mDashBoardFragment = new BlankFragment();
-        return mDashBoardFragment;
+        blankFragment = new BlankFragment();
+        return blankFragment;
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri){
+
+    }
 }
